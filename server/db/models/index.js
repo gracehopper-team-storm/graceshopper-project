317f5c62c5994018b52dd467e2e9a71df188c985
Mollie Davidson <mdavidson13@fordham.edu>
2021-03-03 12:01:38 -0500

    BE: Completes User model, Adds model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,30 @@
 const User = require('./user')
+const Product = require('./product')
+const Order = require('./order')
+const Address = require('./address')
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+const Order_Product = sequelize.define(
+  'Order_Product',
+  {
+    quantity: SEQUELIZE.Integer,
+  },
+  {timestamps: false}
+)
+Product.belongsToMany(Order, {through: Order_Product})
+Order.belongsToMany(Product, {through: Order_Product})
+
+const User_Address = sequelize.define(
+  'User_Address',
+  {
+    type: SEQUELIZE.ENUM(['billing', 'shipping']),
+  },
+  {timestamps: false}
+)
+User.belongsToMany(Address, {through: User_Address})
+Address.belongsToMany(User, {through: User_Address})
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +40,5 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
 }
