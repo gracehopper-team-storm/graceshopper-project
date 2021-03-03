9bfefdc937b6ff277b6d1dea96c0a62f7fb99538
Seonae <seonaebyeon1@gmail.com>
2021-03-03 12:03:03 -0500

    define order module

---
+++ server/db/models/order.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('orders', {
+  session_id: {
+    type: Sequelize.STRING
+  },
+  status: {
+    type: Sequelize.ENUM('in_cart', 'completed')
+  }
+})
+
+module.export = Order
