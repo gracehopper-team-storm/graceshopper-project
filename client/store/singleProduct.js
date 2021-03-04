5d9f2c7c4aa34e783249d58280d812728287d21b
Mollie Davidson <mdavidson13@fordham.edu>
2021-03-04 17:46:22 -0500

    FE: Sets up singleProd react component and redux

---
+++ client/store/singleProduct.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+//action types
+const SET_PRODUCT = 'SET_PRODUCT'
+
+//action creators
+const setProduct = product => ({
+  type: SET_PRODUCT,
+  product
+})
+
+//thunk
+export const fetchProduct = productId => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/allproducts/${productId}`)
+      dispatch(setProduct(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+//reducer
+
+const initialState = {}
+
+const singleProductReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_PRODUCT:
+      return action.product
+
+    default:
+      return state
+  }
+}
+
+export default singleProductReducer
