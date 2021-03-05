7c0948935ed5b21589c1fb9f6b23de766122bd89
Mollie Davidson <mdavidson13@fordham.edu>
2021-03-05 09:20:39 -0500

    BE: Adds group users to enable test logins, Increases number of products seeded to allow for pagination to be implemented, Deletes commented out code, Fixes joinUserAddress bug to successfully seed through-table, moves db.sync to runSeed function, Adds functions to module.exports for testing

--- script/seed.js
+++ script/seed.js
@@ -5,10 +5,44 @@ const {User, Order, Address, Product} = require('../server/db/models')
 const faker = require('faker')
 
 async function seedUsers() {
-  await db.sync({force: true})
-  console.log('db synced!')
+  await User.create({
+    firstName: 'Natalie',
+    lastName: 'Lane',
+    email: 'natalie.lane@gracehopper.com',
+    password: 'ilovedogs123!'
+  })
+  await User.create({
+    firstName: 'Kade',
+    lastName: 'Cahe',
+    email: 'kadecahe@gmail.com',
+    password: 'ilovedogs123!'
+  })
+  await User.create({
+    firstName: 'Jamie',
+    lastName: 'Eunice',
+    email: 'jamieecarrasquillo@gmail.com',
+    password: 'ilovedogs123!'
+  })
+  await User.create({
+    firstName: 'Mollie',
+    lastName: 'Davidson-Blue',
+    email: 'med1198@gmail.com',
+    password: 'ilovedogs123!'
+  })
+  await User.create({
+    firstName: 'Andrea',
+    lastName: 'Crabtree',
+    email: 'andreancrabtree@gmail.com',
+    password: 'ilovedogs123!'
+  })
+  await User.create({
+    firstName: 'Selina',
+    lastName: 'Byeon',
+    email: 'seonaebyeon1@gmail.com',
+    password: 'ilovedogs123!'
+  })
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 25; i++) {
     let firstName = faker.name.firstName()
     let lastName = faker.name.lastName()
     let email = faker.internet.email()
@@ -22,15 +56,12 @@ async function seedUsers() {
     })
   }
 
-  console.log(`seeded 10 users`)
+  console.log(`seeded 31 users`)
   console.log(`seeded successfully`)
 }
 
 async function seedAddresses() {
-  // await db.sync({force: true})
-  // console.log('db synced!')
-
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 25; i++) {
     let streetAddress = faker.address.streetAddress()
     let city = faker.address.city()
     let state = faker.address.state()
@@ -44,29 +75,28 @@ async function seedAddresses() {
     })
   }
 
-  console.log(`seeded 10 addresses`)
+  console.log(`seeded 25 addresses`)
   console.log(`seeded successfully`)
 }
 
-// async function joinUserAddress(userId, addressId) {
-//   // find the user & address
-//   const user = await User.findOne({ where: { id: userId } });
-//   const address = await Address.findOne({ where: { id: addressId } });
-//   // add address and user to the join table with the custom method:
-//   address.addUser(user);
-// }
+async function joinUserAddress(userId, addressId) {
+  // find the user & address
+  const user = await User.findOne({where: {id: userId}})
+  const address = await Address.findOne({where: {id: addressId}})
+  // add address and user to the join table with the custom method:
+  await address.addUser(user)
+}
 
-// async function associatesUserAddress(){
-//   for(let i = 0; i < 10; i++){
-//     await joinUserAddress(i, 4)
-//   }
-// }
+async function associatesUserAddress() {
+  for (let i = 0; i < 10; i++) {
+    await joinUserAddress(i, 4)
+  }
+  console.log(`associated 10 user-addresses`)
+  console.log(`associated successfully`)
+}
 
 async function seedProducts() {
-  // await db.sync({force: true})
-  // console.log('db synced!')
-
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 100; i++) {
     let name = faker.commerce.productName()
     let description = faker.commerce.productDescription()
     let image = faker.image.nature()
@@ -82,7 +112,7 @@ async function seedProducts() {
     })
   }
 
-  console.log(`seeded 25 products`)
+  console.log(`seeded 100 products`)
   console.log(`seeded successfully`)
 }
 
@@ -92,10 +122,12 @@ async function seedProducts() {
 async function runSeed() {
   console.log('ing...')
   try {
+    await db.sync({force: true})
+    console.log('db synced!')
     await seedUsers()
     await seedAddresses()
     await seedProducts()
-    //await associatesUserAddress()
+    await associatesUserAddress()
   } catch (err) {
     console.error(err)
     process.exitCode = 1
@@ -114,4 +146,4 @@ if (module === require.main) {
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = {seedAddresses, seedUsers}
+module.exports = {seedAddresses, seedUsers, seedProducts, joinUserAddress}
