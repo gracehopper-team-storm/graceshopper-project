7ee7f4ba889765d46f854d5f092dea6711b1ad07
Seonae <seonaebyeon1@gmail.com>
2021-03-05 22:04:29 -0500

    FE: edit signin and sign up

--- client/components/index.js
+++ client/components/index.js
@@ -5,5 +5,6 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
-export {Login, Signup} from './auth-form'
+export {Login} from './auth-form'
+export {Signup} from './Signup'
 export {default as AllProducts} from './AllProducts'
