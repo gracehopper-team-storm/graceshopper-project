c429fe5fa5f8e5964088c9256fceec62e0f4493c
Andrea Crabtree <andreancrabtree@gmail.com>
2021-03-08 14:08:09 -0500

    Creates products array in cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,21 +3,25 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {findOrCreateCart} from '../store/cart'
+import {fetchProduct} from '../store/singleProduct'
 
 class Cart extends React.Component {
-  componentDidMount() {
-    this.props.findOrCreateCart(this.props.userId)
-  }
+  // componentDidMount() {
+  //   this.props.findOrCreateCart(this.props.userId)
+  // }
   render() {
-    const {order} = this.props
+    const products = this.props.order.map(elem => {
+      fetchProduct(elem.productId)
+    })
+    console.log(products)
     return (
       <div id="cart">
         <h2>Cart</h2>
         <div id="order-container">
-          {!order.length || order === undefined ? (
+          {!products.length || products === undefined ? (
             <h3>Empty</h3>
           ) : (
-            order.map(item => (
+            products.map(item => (
               <div key={item.id} id="item-container">
                 <div id="item">
                   <img src={item.imageUrl} alt={item.name} />
@@ -31,20 +35,20 @@ class Cart extends React.Component {
                     <button
                       id="increase"
                       onClick={() => {
-                        this.props.increaseQuantity(order.id, item.id)
+                        this.props.increaseQuantity()
                       }}
                     />
                     {item.quantity}
                     <button
                       id="decrease"
                       onClick={() => {
-                        this.props.decreaseQuantity(order.id, item.id)
+                        this.props.decreaseQuantity()
                       }}
                     />
                     <button
                       id="delete-product"
                       onClick={() => {
-                        this.props.deleteProduct(order.id, item.id)
+                        this.props.deleteProduct()
                       }}
                     >
                       Delete Item
@@ -70,9 +74,10 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     findOrCreateCart: userId => dispatch(findOrCreateCart(userId)),
-    increaseQuantity: () => dispatch(increaseQuantity(orderId, productId)),
-    decreaseQuantity: () => dispatch(decreaseQuantity(orderId, productId)),
-    deleteProduct: () => dispatch(deleteProduct(orderId, productId))
+    fetchProduct: productId => dispatch(fetchProduct(productId))
+    // increaseQuantity: () => dispatch(increaseQuantity(orderId, productId)),
+    // decreaseQuantity: () => dispatch(decreaseQuantity(orderId, productId)),
+    // deleteProduct: () => dispatch(deleteProduct(orderId, productId))
   }
 }
 
