556c79bc0208a8a97597cb0ff98135de243b1278
Mollie Davidson <mdavidson13@fordham.edu>
2021-03-09 21:30:28 -0500

    FE: fixes build-fail

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-// import {findOrCreateCart} from '../store/cart'
 import LocalCart from './LocalCart'
 import DeleteButton from './DeleteButton'
 import IncrementButton from './IncrementButton'
